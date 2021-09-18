b2239d59d466e01c2df85cf0834c9518d7383535
Thomas Mafut <89086033+thomas64mafut@users.noreply.github.com>
2021-09-18 18:31:11 -0300

    Implementacion de ArrayLists

--- src/principal/controladores/ControladorPrincipal.java
+++ src/principal/controladores/ControladorPrincipal.java
@@ -1,7 +1,23 @@
 package principal.controladores;
 
+import autores.modelos.Alumno;
+import autores.modelos.Profesor;
+import grupos.modelos.Grupo;
+import idiomas.modelos.Idioma;
+import lugares.modelos.Lugar;
+import palabrasclaves.modelos.PalabraClave;
+import tipos.modelos.Tipo;
+import java.util.ArrayList;
+
 public class ControladorPrincipal {
     public static void main(String[] args) {
+        ArrayList<Alumno> alumnos = new ArrayList<>();
+        ArrayList<Profesor> profesores = new ArrayList<>();
+        ArrayList<Idioma> idiomas = new ArrayList<>();
+        ArrayList<Lugar> lugares = new ArrayList<>();
+        ArrayList<PalabraClave> palabrasClaves = new ArrayList<>();
+        ArrayList<Tipo> tipos = new ArrayList<>();
+        ArrayList<Grupo> grupos = new ArrayList<>();
 
     }
 }
