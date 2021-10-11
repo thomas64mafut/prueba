74a43a794c3648e9b810e8b7df8548532e571de9
Luis0381 <luiseugeniomr@gmail.com>
2021-10-11 17:41:43 -0300

    Avance en clases "MiembroEnGrupo" y "Publicacion"

--- src/grupos/modelos/MiembroEnGrupo.java
+++ src/grupos/modelos/MiembroEnGrupo.java
@@ -1,4 +1,11 @@
 package grupos.modelos;
 
+import autores.modelos.Profesor;
+
+import java.time.LocalDate;
+
 public class MiembroEnGrupo {
+    private Profesor profesor;
+    private Rol rol;
+    private Grupo grupo;
 }
--- src/publicaciones/modelos/Publicacion.java
+++ src/publicaciones/modelos/Publicacion.java
@@ -1,4 +1,24 @@
 package publicaciones.modelos;
 
+import grupos.modelos.MiembroEnGrupo;
+import idiomas.modelos.Idioma;
+import lugares.modelos.Lugar;
+import palabrasclaves.modelos.PalabraClave;
+import tipos.modelos.Tipo;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
 public class Publicacion {
+    private String titulo;
+    private LocalDate fechaPublicacion;
+    private String enlace;
+    private String resumen;
+
+
+    private ArrayList<PalabraClave> palabrasClaves = new ArrayList<>();
+    private MiembroEnGrupo autor;
+    private Lugar lugarPublicacion;
+    private Idioma idiomaPublicacion;
+    private Tipo tipoPublicacion;
 }
