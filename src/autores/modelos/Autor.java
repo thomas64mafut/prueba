f9233e8222a9879bd1db7bd55267305afa939842
Thomas <mafut50@gmail.com>
2021-10-21 18:57:52 -0300

    Pt. 2.5 - Clase Autor

---
+++ src/autores/modelos/Autor.java
@@ -0,0 +1,83 @@
+package autores.modelos;
+
+import grupos.modelos.MiembroEnGrupo;
+import java.util.ArrayList;
+import java.util.Objects;
+
+public abstract class Autor {
+    private int dni;
+    private String apellidos;
+    private String nombres;
+    private String clave;
+    
+    private ArrayList <MiembroEnGrupo> miembroengrupos;
+
+
+    public Autor(int dni, String apellidos, String nombres, String clave) {
+        this.dni = dni;
+        this.apellidos = apellidos;
+        this.nombres = nombres;
+        this.clave = clave;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 43 * hash + this.dni;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass().getSuperclass() != obj.getClass().getSuperclass()) {
+            return false;
+        }
+        final Autor other = (Autor) obj;
+        if (this.dni != other.dni) {
+            return false;
+        }
+        return true;
+    }
+
+    public void mostrar(){
+        System.out.println("[" + dni + "] " + apellidos + ", " + nombres);
+    }
+
+    public int verDni() {
+        return dni;
+    }
+
+    public void asignarDni(int dni) {
+        this.dni = dni;
+    }
+
+    public String verApellidos() {
+        return apellidos;
+    }
+
+    public void asignarApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String verNombres() {
+        return nombres;
+    }
+
+    public void asignarNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String verClave() {
+        return clave;
+    }
+
+    public void asignarClave(String clave) {
+        this.clave = clave;
+    }
+}
